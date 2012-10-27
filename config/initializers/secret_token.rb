971699e137dd6abe202f75eb2f3536788ddf8349
Pierre Lasante <pierre.lasante@videotron.ca>
2012-10-27 16:53:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monepicerie::Application.config.secret_token = '1c5c6cb19dc07a33e48bc2d62eb7e453239a03340b86075df55b129ca6c2c35a058efbf6f0db850abc20d7eed9727e21b6c11fd25a8f37b323077ec56a4abe18'
