c0e0fed32a0583a2d5f4d43ba7a30a30e9255925
Pierre Lasante <pierre.lasante@videotron.ca>
2012-10-28 11:09:36 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,31 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject {page}
   describe "Home page" do
     it "should have the content 'My Grocery'" do
       visit home_path
-      page.should have_content('My Grocery')
+      should have_content('My Grocery')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
       visit help_path
-      page.should have_content('Help')
+      should have_content('Help')
     end
   end
   
   describe "About page" do
     it "should have the content 'About Us'" do
       visit about_path
-      page.should have_content('About Us')
+      should have_content('About Us')
     end
   end
   
   describe "Contact page" do
     it "should have the H1 'Contact'" do
       visit contact_path
-      page.should have_selector("h1", :text => 'Contact')
+      should have_selector("h1", :text => 'Contact')
     end
   end
 end
