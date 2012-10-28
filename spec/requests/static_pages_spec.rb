1014fae71e2df805904d01bf7e761442f93a3dc1
Pierre Lasante <pierre.lasante@videotron.ca>
2012-10-28 10:37:34 -0400

    Added the contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,22 +3,29 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'My Grocery'" do
-      visit '/static_pages/home'
+      visit home_path
       page.should have_content('My Grocery')
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
   
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the H1 'Contact'" do
+      visit contact_path
+      page.should have_selector("h1", :text => 'Contact')
     end
   end
 end
