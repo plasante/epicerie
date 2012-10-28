41b355218018431b7c8e77eb70614b5cd1819a1d
Pierre Lasante <pierre.lasante@videotron.ca>
2012-10-28 08:04:33 -0400

    J'ai fait d'autres modifications pour dealer avec la locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,18 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale_from_browser
 
   
-  # il faut absolument fermer le browser pour killer la variable session[:initialized]
+  # Il faut absolument fermer le browser pour killer la variable session[:initialized]
   # et reinitialiser I18n.locale
+  # http://localhost:3000/static_pages/home?locale=en-us
+  # devrait changer la locale a 'en-us'
   
   def set_locale_from_browser
     if (session[:initialized].nil? || !session[:initialized])
       logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
       I18n.locale = extract_locale_from_accept_language_header
       logger.debug "* Locale set to '#{I18n.locale}'"
+    elsif (!params[:locale].nil?)
+      I18n.locale = params[:locale]
     else
       logger.debug "* Locale already set to '#{I18n.locale}'"
     end
@@ -20,11 +24,11 @@ class ApplicationController < ActionController::Base
   private
   
   def extract_locale_from_accept_language_header
+    # Ce test est necessaire pour les test unitaires car request.env['HTTP_ACCEPT_LANGUAGE'] est nil
     if (!request.env['HTTP_ACCEPT_LANGUAGE'].nil?)
-      # This test is necessary for the unit test
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      request.env['HTTP_ACCEPT_LANGUAGE'].split(',').first
     else
-      # This the default locale
+      # Valeur par defaut
       'en'
     end
   end
