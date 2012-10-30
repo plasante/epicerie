ee4f3fa0f494fbb5d6bf11073e2279dbdc353751
Pierre Lasante <pierre.lasante@videotron.ca>
2012-10-30 16:27:01 -0400

    reached 7.2.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,4 +17,32 @@ describe "UserPages" do
     it { should have_selector("h2", :text => user.first_name) }
     it { should have_selector("h2", :text => user.last_name) }
   end
+  
+  describe "signup page" do
+    before { visit signup_path }
+    
+    let(:submit) { "Create my account" }
+    
+    describe "with invalid information>" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "first_name",               :with => "Pierre"
+        fill_in "last_name",                :with => "Lasante"
+        fill_in "username",                 :with => "plasante"
+        fill_in "email",                    :with => "plasante@email.com"
+        fill_in "password",                 :with => "123456"
+        fill_in "password_confirmation",    :with => "123456"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+    
+  end
 end
