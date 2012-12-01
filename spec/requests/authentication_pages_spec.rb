6c9de2b7b15e9f60448a8477b97cadf4debf71c5
Pierre Lasante <pierre.lasante@videotron.ca>
2012-12-01 11:56:17 -0500

    I've rearranged authentication_pages_spec.rb so that all the text reference are relative to I18n.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
     
-    it { should have_selector('h1', :text => 'Sign in')}
+    it { should have_selector('h1', :text => I18n.t(:url_sign_in))}
   end
   
   describe "signin" do
     before { visit signin_path }
     
     describe "with invalid information" do
-      before { click_button :url_sign_in }
+      before { click_button I18n.t(:url_sign_in) }
       
-      it { should have_selector('h1', :text => 'Sign in')}
-      it { should have_selector('div.alert.alert-error', :text => 'Invalid')}
+      it { should have_selector('h1', :text => I18n.t(:url_sign_in))}
+      it { should have_selector('div.alert.alert-error', :text => I18n.t(:invalid_email_password_combination))}
       
       # Il faut utiliser flash.now pour faire passer ce test.
       describe "after visiting another page" do
@@ -28,14 +28,19 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email", :with => user.email
-        fill_in "Password", :with => user.password
-        click_button :url_sign_in
+        fill_in I18n.t(:user_email), :with => user.email
+        fill_in I18n.t(:user_password), :with => user.password
+        click_button I18n.t(:url_sign_in)
       end
       
       it { should have_link( I18n.t(:url_sign_out), :href => signout_path(user)) }
       it { should_not have_link( I18n.t(:url_sign_in), :href => signin_path) }
       
+      describe "followed by signout" do
+        before { click_link I18n.t(:url_sign_out) }
+        it { should_not have_link( I18n.t(:url_sign_out), :href => signout_path(user)) }
+        it { should have_link( I18n.t(:url_sign_in), :href => signin_path) }
+      end
     end
   end
 end
