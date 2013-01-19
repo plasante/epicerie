059e3c25e42d5bf5c2ec572ae43b7d888b9b9887
Pierre Lasante <pierre.lasante@videotron.ca>
2013-01-19 12:53:18 -0500

    Je recommence mes modeles de produit

--- db/migrate/20130113150008_create_magasin_types.rb
+++
@@ -1,9 +0,0 @@
-class CreateMagasinTypes < ActiveRecord::Migration
-  def change
-    create_table :magasin_types do |t|
-      t.string :nom
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130113150330_create_manufacturers.rb
+++
@@ -1,9 +0,0 @@
-class CreateManufacturers < ActiveRecord::Migration
-  def change
-    create_table :manufacturers do |t|
-      t.string :nom
-
-      t.timestamps
-    end
-  end
-end
