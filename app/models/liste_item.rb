713b7f70cd6f7527a92cf282755ee25faaeecba0
Pierre Lasante <pierre.lasante@videotron.ca>
2013-01-27 11:45:12 -0500

    Presque termine les associations de liste_items

--- app/models/liste_item.rb
+++ app/models/liste_item.rb
@@ -2,5 +2,9 @@ class ListeItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :produit
   
-  attr_accessible :qty
+  attr_accessible :qty, :user, :produit
+
+  validates :user, :presence => true
+  validates :produit, :presence => true
+  validates :qty, :numericality => true
 end
--- spec/models/liste_item_spec.rb
+++ spec/models/liste_item_spec.rb
@@ -1,5 +1,74 @@
 require 'spec_helper'
 
 describe ListeItem do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:produit_nom) { FactoryGirl.create(:produit_nom, :nom => 'Lait ecreme') }
+  let!(:category) { FactoryGirl.create(:category, :nom => 'Produits Laitiers') }
+  let!(:fabricant) { FactoryGirl.create(:fabricant, :nom => 'Quebon') }
+  let!(:format) { FactoryGirl.create(:format, :nom => '2L') }
+
+  let!(:produit) { FactoryGirl.create(:produit,
+  									  :produit_nom => produit_nom,
+  									  :category => category,
+  									  :fabricant => fabricant,
+  									  :format => format) }
+
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before {
+  	@liste_item = ListeItem.new(:user => user,
+  								:produit => produit,
+  								:qty => 1)
+  }
+
+  subject { @liste_item }
+
+  it { should be_valid }
+
+  describe "attributes" do
+  	it { should respond_to(:qty) }
+  end
+
+  describe "methods" do
+  	it { should respond_to(:user) }
+  	it { should respond_to(:produit) }
+  end
+
+  describe "validations" do
+  	describe "when user is nil" do
+  	  before { @liste_item.user = nil }
+  	  it { should_not be_valid }
+  	end
+  	describe "when produit is nil" do
+  	  before { @liste_item.produit = nil }
+  	  it { should_not be_valid }
+  	end
+  	describe "for qty" do
+  	  describe "when not a number" do
+  	  	before { @liste_item.qty = 'a' }
+  	  	it { should_not be_valid }
+  	  end
+  	end
+  end
+
+  describe "associations" do
+  	describe "with users" do
+  	  it "username" do
+  	  	@liste_item.user.username.should == user.username
+  	  end
+  	end
+  	describe "with produits" do
+  	  it "produit_nom.nom" do
+  	  	@liste_item.produit.produit_nom.nom.should == produit_nom.nom
+  	  end
+  	  it "category.nom" do
+  	  	@liste_item.produit.category.nom.should == category.nom
+  	  end
+  	  it "fabricant.nom" do
+  	  	@liste_item.produit.fabricant.nom.should == fabricant.nom
+  	  end
+  	  it "format.nom" do
+  	  	@liste_item.produit.format.nom.should == format.nom
+  	  end
+  	end
+  end
 end
