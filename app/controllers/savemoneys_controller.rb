d1369d2ea752843d7c6475b3a0e8ba9477fea0b8
Pierre Lasante <pierre.lasante@videotron.ca>
2013-03-03 09:13:14 -0500

    Complété la partie qui recherche le meilleur prix. Il me faut maintenant populer la db et tester le tout. Il faut aussi que la recherche du meilleur prix fonctionne meme si l'usager n'est pas connecter ou enregistrer.

--- app/controllers/savemoneys_controller.rb
+++ app/controllers/savemoneys_controller.rb
@@ -1,7 +1,6 @@
 class SavemoneysController < ApplicationController
   def create
-    @results = MagasinProduit.all(:limit => 3)
-    @results2 = ListeItem.getMeilleurPrix(current_user)
+    @results = MagasinProduit.getMeilleurPrix(current_user)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js
--- app/models/magasin_produit.rb
+++ app/models/magasin_produit.rb
@@ -8,4 +8,20 @@ class MagasinProduit < ActiveRecord::Base
   validates :produit, :presence => true
   validates :prix_regulier, :numericality => true
   validates :prix_special, :numericality => true
+
+  #
+  # Une liste de magasins ayant le plus bas prix sera affiche a l'ecran.
+  #
+  def self.getMeilleurPrix( user )
+  	magasin_produits = []
+  	liste_items = ListeItem.select("produit_id").where("user_id = :user_id", :user_id => user).map(&:produit_id)
+    # Pour trouver le meilleur prix pour lait_ecreme 1% 2% et 3.25% en format 2L
+    #liste_items = [2,14,26,38]
+    liste_items.each do |liste_item|
+  	  produit = Produit.select("produit_nom_id, format_id").where("id = #{liste_item}")
+      sous_select = "SELECT id FROM produits WHERE produit_nom_id = #{produit[0][:produit_nom_id]} AND format_id = #{produit[0][:format_id]}"
+	  magasin_produits << MagasinProduit.where("produit_id IN (#{sous_select})").order("prix_regulier ASC").limit("1")
+  	end
+    magasin_produits
+  end
 end
